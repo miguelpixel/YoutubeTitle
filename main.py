bf49d82a4059c545ed83d082590f07f629ea70cc
miguelpixel <miguel.roses.m@gmail.com>
2023-07-27 23:58:14 -0700

    primer comentario

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+from flask import Flask, request, url_for , make_response, redirect, render_template
+from dotenv import load_dotenv
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import YoutubeLoader
+from langchain.chains.summarize import load_summarize_chain
+from langchain.llms import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+from langchain.llms import OpenAI
+
+app = Flask(__name__)
+
+todos = ['TODO 1','TODO 2','TODO 3']
+
+@app.route('/', methods=('GET','POST'))
+def index():
+    if request.method == 'POST':
+        url = request.form['url']
+        loader = YoutubeLoader.from_youtube_url(url, add_video_info=True, language = 'es')
+        result = loader.load()
+        
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000,chunk_overlap=0)
+        texts = text_splitter.split_documents(result)
+        texts_trim = texts[:5]
+        llm = OpenAI(temperature=0.2)
+        prompt_template = """Eres un experto en Copywriting, Creame 5 titulos de máximo 50 caracteres con el siguiente texto:
+
+        text: ```{text}```
+
+        """
+        PROMPT = PromptTemplate(template=prompt_template,input_variables=["text"])
+        chain = load_summarize_chain(llm, chain_type="stuff", prompt=PROMPT)
+        chain.run(texts_trim)
+
+        return render_template('index.html',result = chain.run(texts_trim))
+        
+
+    #user_ip = request.remote_addr
+    #response = make_response(redirect('/hello'))
+    #response.set_cookie('user_ip',user_ip)
+    result = request.args.get('url')
+    return render_template('index.html', result=result)
+    
+
+
+@app.route('/hello', methods=('GET','POST'))
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip' : user_ip,
+        'todos' : todos,
+
+
+    }
+    return render_template('hello.html', **context)
